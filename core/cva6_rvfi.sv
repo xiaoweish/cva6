ba051ef8949931af8a64d7580eb835b7a2a83392
Mario <mario@openhwgroup.org>
2024-05-23 12:39:38 +0000

    Get Questa testharness working and add it to CI

--- core/cva6_rvfi.sv
+++ core/cva6_rvfi.sv
@@ -370,7 +370,7 @@ module cva6_rvfi
 
   `CONNECT_RVFI_FULL(1'b1, menvcfg, csr.fiom_q)
 
-  `CONNECT_RVFI_FULL(CVA6Cfg.XLEN == 32, menvcfgh, 0)
+  `CONNECT_RVFI_FULL(CVA6Cfg.XLEN == 32, menvcfgh, 32'h0)
 
   `CONNECT_RVFI_FULL(1'b1, mvendorid, OPENHWGROUP_MVENDORID)
   `CONNECT_RVFI_FULL(1'b1, marchid, ARIANE_MARCHID)
